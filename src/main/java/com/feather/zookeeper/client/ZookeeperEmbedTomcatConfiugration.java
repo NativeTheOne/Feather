41ebb8b53bee50369bbc176f89a677010e2d549a
NativeTheOne <354448207@qq.com>
2018-12-29 10:34:33 +0800

    canal project client

--- src/main/java/com/feather/zookeeper/client/ZookeeperEmbedTomcatConfiugration.java
+++ src/main/java/com/feather/zookeeper/client/ZookeeperEmbedTomcatConfiugration.java
@@ -1,6 +1,8 @@
 package com.feather.zookeeper.client;
 
+import com.feather.Person;
 import com.google.gson.Gson;
+import org.apache.catalina.startup.Tomcat;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.RetrySleeper;
 import org.apache.curator.framework.CuratorFramework;
@@ -17,72 +19,11 @@ public class ZookeeperEmbedTomcatConfiugration {
 
     private static Gson gson = new Gson();
 
-    private static class TomcatConfiguration{
-
-        private int port;
-
-        private String tempDir;
-
-        private String appBase;
-
-        private String webAppContextPath;
-
-        private String webAppDocBase;
-
-        public TomcatConfiguration(int port, String tempDir, String appBase, String webAppContextPath, String webAppDocBase) {
-            this.port = port;
-            this.tempDir = tempDir;
-            this.appBase = appBase;
-            this.webAppContextPath = webAppContextPath;
-            this.webAppDocBase = webAppDocBase;
-        }
-
-        public int getPort() {
-            return port;
-        }
-
-        public void setPort(int port) {
-            this.port = port;
-        }
-
-        public String getTempDir() {
-            return tempDir;
-        }
-
-        public void setTempDir(String tempDir) {
-            this.tempDir = tempDir;
-        }
-
-        public String getAppBase() {
-            return appBase;
-        }
-
-        public void setAppBase(String appBase) {
-            this.appBase = appBase;
-        }
-
-        public String getWebAppContextPath() {
-            return webAppContextPath;
-        }
-
-        public void setWebAppContextPath(String webAppContextPath) {
-            this.webAppContextPath = webAppContextPath;
-        }
-
-        public String getWebAppDocBase() {
-            return webAppDocBase;
-        }
-
-        public void setWebAppDocBase(String webAppDocBase) {
-            this.webAppDocBase = webAppDocBase;
-        }
-    }
-
     private static CuratorFramework zookeeperClient;
 
-    private static String listenPath = "/tomcat/configuration";
+    private static String listenPath = "/configuration";
 
-    private static int port = 8080;
+    private static NodeCache nodeCache;
 
     static{
         zookeeperClient = CuratorFrameworkFactory.newClient("127.0.0.1:2181", 3000, 2000, new RetryPolicy() {
@@ -99,28 +40,27 @@ public class ZookeeperEmbedTomcatConfiugration {
                 return false;
             }
         });
-
     }
 
     /*
     判断节点是新增、修改还是删除应该通过PathChildrenCache来处理，PathChildrenCacheListener有event参数，
     NodeCache和NodeCacheListener是监听节点内容变更的，直接通过cn.getCurrentData().data()可以获得节点最新内容
      */
-
     public static void main(String[] args) throws Exception {
         zookeeperClient.start();
-        TomcatConfiguration configuration = new TomcatConfiguration(port, File.createTempFile("tomcat.","."+port).getAbsolutePath(),".","",".");
-        String configurationJson = gson.toJson(configuration);
-        zookeeperClient.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(listenPath,configurationJson.getBytes());
-        NodeCache nodeCache = new NodeCache(zookeeperClient,listenPath);
+        nodeCache = new NodeCache(zookeeperClient,listenPath);
+        Person person = new Person("theone",23,"theone");
+        String personJson = gson.toJson(person);
         nodeCache.getListenable().addListener(new NodeCacheListener() {
             @Override
             public void nodeChanged() throws Exception {
-                byte[] bs = nodeCache.getCurrentData().getData();
-                String newConfiguration = new String(bs);
-                TomcatConfiguration newTomcatConfig = gson.fromJson(newConfiguration,TomcatConfiguration.class);
+                Person person = gson.fromJson(new String(nodeCache.getCurrentData().getData()),Person.class);
+                System.out.println("New Data:"+person);
             }
         });
+        nodeCache.start();
+        zookeeperClient.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(listenPath,personJson.getBytes());
+        TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
     }
 
 }
